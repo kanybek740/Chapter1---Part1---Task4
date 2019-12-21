4b90d7a55740c3c1547cac3f2d7b61e329f1fecc
kanybek740 <kanybekk1992@gmail.com>
2019-12-21 11:28:24 +0600

    Chapter1_Part1_Task4

---
+++ task4.py
@@ -0,0 +1,18 @@
+# Write a function last_letter that returns the last letter of a string.
+# Ex: last_letter('hello!') # --> !
+
+def last_letter(w):
+    return (w[-1])
+    
+
+
+#write your code here...
+#DO NOT remove lines below here, this is designed to test your code.
+def test_last_letter():
+    assert last_letter('hello!') == '!'
+    assert last_letter('banana') == 'a'
+    assert last_letter('8') == '8'
+    assert last_letter('funnyguys') == 's'
+    assert last_letter('101') == '1'
+    print("YOUR CODE IS CORRECT!")
+test_last_letter()
